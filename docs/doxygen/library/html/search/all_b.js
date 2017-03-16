3e3d6f9466a0f7ad66e9c387bdfd35a428d3628b
Shun Moriya <shun@mnu.sakura.ne.jp>
2017-03-17 01:51:14 +0900

    add pages

---
+++ docs/doxygen/library/html/search/all_b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['offset',['offset',['../structmana__datalink__info__header.html#a894bdfa2d603d8343f8ef01dda6fcd23',1,'mana_datalink_info_header']]]
+];
---
+++ docs/doxygen/library/html/search/files_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['libmana_2ec',['libmana.c',['../libmana_8c.html',1,'']]],
+  ['libmana_2eh',['libmana.h',['../libmana_8h.html',1,'']]]
+];
