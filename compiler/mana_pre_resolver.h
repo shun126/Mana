6cc523fcd09a478bf4452dbc52bb79af3a5922cf
Shun Moriya <shun@mnu.sakura.ne.jp>
2017-03-31 02:43:06 +0900

    update doxygen, resolve symbol node functions

--- compiler/mana_pre_resolver.h
+++ compiler/mana_pre_resolver.h
@@ -1,15 +1,15 @@
 /*
  * mana (compiler)
  *
- * @file	mana_evaluator.h
+ * @file	mana_pre_resolver.h
  * @brief	構文木評価に関するヘッダファイル
  * @detail	このファイルは構文木評価に関係するヘッダファイルです。
  * @author	Shun Moriya
  * @date	2017-
  */
 
-#if !defined(___MANA_EVALUATOR_H___)
-#define ___MANA_EVALUATOR_H___
+#if !defined(___MANA_PRE_RESOLVER_H___)
+#define ___MANA_PRE_RESOLVER_H___
 
 #if !defined(___MANA_SYMBOL_H___)
 #include "mana_symbol.h"
@@ -20,22 +20,22 @@ extern "C" {
 #endif
 
 //! 初期化
-extern void mana_evaluator_initialize(void);
+extern void mana_pre_resolver_initialize(void);
 
 //! 終了
-extern void mana_evaluator_finalize(void);
+extern void mana_pre_resolver_finalize(void);
 
 /*!
 子ノードから型を継承する
 @param[in]	node	ノード
 */
-extern void mana_evaluator_inherit_type_from_child_node(mana_node* node);
+extern void mana_resolver_resolve_type_from_child_node(mana_node* node);
 
 /*!
 ノードを辿りながらシンボル情報を登録します（ただし中間言語に対して評価しません）
 @param	node	ノード
 */
-extern void mana_evaluator_evaluate(mana_node* node);
+extern void mana_pre_resolver_resolve(mana_node* node);
 
 #if defined(_LANGUAGE_C_PLUS_PLUS) || defined(__cplusplus) || defined(c_plusplus)
 }
