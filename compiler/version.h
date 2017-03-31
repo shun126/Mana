e063911880062f96e4695853a039bd872fa2dc9f
Shun Moriya <shun@mnu.sakura.ne.jp>
2017-04-01 03:04:49 +0900

    update doxygen and remove prefix from filename

---
+++ compiler/version.h
@@ -0,0 +1,20 @@
+/*
+ * mana (compiler)
+ *
+ * @file	mana_version.h
+ * @brief	バージョン情報に関するヘッダーファイル
+ * @detail	このファイルはバージョン情報に関するヘッダーファイルです。
+ * @author	Shun Moriya <shun@mnu.sakura.ne.jp>
+ * @date	2003-
+ */
+
+#if !defined(___VERSION_H___)
+#define ___VERSION_H___
+
+#define BUILD_YEAR 2017
+#define BUILD_MONTH 3
+#define BUILD_DAY 31
+#define BUILD_MAJOR_VERSION "0"
+#define BUILD_MINOR_VERSION "9.6"
+
+#endif
